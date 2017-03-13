cb432cbf3b2420a33151143a5161b69fa8efb6c7
logics-layout <git@logics.su>
2017-03-14 02:39:24 +0800

    Почти закончил шапку

--- markup/components/header/header.js
+++ markup/components/header/header.js
@@ -1,79 +1,74 @@
-// var _fnMenuHeaderTop = function(toggle, list, slide) {
-//     var className, el, time;
-//     time = 300;
-//     el = toggle.closest('.mobile-list-anim');
-//     className = 'active';
-//
-//     if (toggle.prop("checked")) {
-//         if(slide) list.stop().slideDown(time);
-//         el.add(list).addClass(className);
-//     } else {
-//         if(slide) list.stop().slideUp(time);
-//         el.add(list).removeClass(className);
-//     }
-// };
-//
-// var menuHeaderTopToggle = $('#header-bottom-toggleMenuMbl');
-// var menuHeaderTopList = $('.header-bottom-nav');
-//
-// menuHeaderTopToggle.change(function() {_fnMenuHeaderTop(menuHeaderTopToggle, menuHeaderTopList, true);});
-// _fnMenuHeaderTop(menuHeaderTopToggle, menuHeaderTopList, true);
-// $(document).click(function(e) {
-//     var target;
-//     target = $(e.target);
-//     if ((!target.is(menuHeaderTopList) && !target.closest(menuHeaderTopList)[0]) && (!target.is(menuHeaderTopToggle) && !target.is('.menuTopMobel'))) {
-//         menuHeaderTopToggle[0].checked = false;
-//         _fnMenuHeaderTop(menuHeaderTopToggle, menuHeaderTopList, true);
-//     }
-// });
-//
-// $('.header-bottom-nav a').click(function(e){
-//     if($(document).width() > 991) return true;
-//     var _this = $(this),
-//         list = _this.next('ul'),
-//         parent = _this.parent(),
-//         className = 'active',
-//         time = 300;
-//
-//     if(list[0]){
-//         e.preventDefault();
-//         parent.toggleClass(className);
-//         list.slideToggle(time);
-//
-//         parent.siblings().removeClass(className)
-//             .find('ul').slideUp(time);
-//     }
-// });
-//
-// //
-// var header = $('.header'),
-//     headerBottomFix = $('.header-bottom-fix'),
-//     checkPad = true;
-//
-// window.headerHeight = _heightBlock(header);
-// var checkHeaderFix = function(){
-//     // return false;
-//
-//     if($(window).width() < 991){
-//
-//         if($(window).scrollTop() > headerBottomFix.offset().top){
-//             header.addClass('fix');
-//         }else{
-//             header.removeClass('fix');
-//         }
-//
-//         //     if(checkPad){
-//         //         header.addClass('fix');
-//         //         header.css({marginTop: "-"+headerHeight+"px"}).animate({marginTop: "0"}, 500);
-//         //         page__wrapper.css('padding-top', headerHeight+"px");
-//         //         checkPad = false;
-//         //     }
-//         // }else if(checkPad===false){
-//         //     checkPad = true;
-//         //     header.removeClass('fix');
-//         //     page__wrapper.css('padding-top', 0+"px");
-//         //     $('.header-slider-el').slick('slickNext');
-//         //     window.headerHeight = _heightBlock(header);
-//         // }
-//     }
-// };
+var _fnMenuHeaderTop = function(toggle, list, slide) {
+    var className, el, time;
+    time = 300;
+    el = toggle.closest('.mobile-list-anim');
+    className = 'active';
+
+    if (toggle.prop("checked")) {
+        if(slide) list.stop().slideDown(time);
+        el.add(list).addClass(className);
+    } else {
+        if(slide) list.stop().slideUp(time);
+        el.add(list).removeClass(className);
+    }
+};
+
+var menuHeaderTopToggle = $('#bottomToggleMenuMobile');
+var menuHeaderTopList = $('.headerMobile__navBlock, .toggleNav');
+
+menuHeaderTopToggle.change(function() {_fnMenuHeaderTop(menuHeaderTopToggle, menuHeaderTopList, false);});
+_fnMenuHeaderTop(menuHeaderTopToggle, menuHeaderTopList, false);
+
+// пуневерсальный переключатель для шапки
+var headerMobileSectionsSection = $('.headerMobile__sections-section'),
+    headerMobileClassName = 'active';
+
+
+
+$(document).click(function(e) {
+    var target;
+    target = $(e.target);
+    if ((!target.is(menuHeaderTopList) && !target.closest(menuHeaderTopList)[0]) && (!target.is(menuHeaderTopToggle) && !target.is('.menuTopMobel'))) {
+        menuHeaderTopToggle[0].checked = false;
+        _fnMenuHeaderTop(menuHeaderTopToggle, menuHeaderTopList, false);
+    }
+
+    var sectionActive = headerMobileSectionsSection.filter('.active');
+    if(sectionActive[0]){
+        var sections = sectionActive.siblings();
+        sections.removeClass(headerMobileClassName).each(function(){
+            var _this = $(this);
+                block = _this.data('block');
+            if(block) {
+                block = $(block);
+                if (block[0]){
+                    block.slideUp(300);
+                }
+            }
+        });
+    }
+
+    // var sectionEl = target.hasClass('headerMobile__sections-section') ? target
+
+    // console.log(target[0]);
+});
+
+headerMobileSectionsSection.click(function () {
+    var _this = $(this),
+        block = _this.data('block'),
+        time = 300;
+
+    if(block){
+        block = $(block);
+        if(block[0]){
+            if(_this.hasClass(headerMobileClassName)){
+                _this.add(headerMobileSectionsSection).removeClass(headerMobileClassName);
+                block.stop().slideUp(time);
+            }else{
+                _this.addClass(headerMobileClassName);
+                _this.siblings().removeClass(headerMobileClassName);
+                block.stop().slideDown(time);
+            }
+        }
+    }
+});
--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -41,18 +41,18 @@ if ($.fn.slick) {
 //     }).on('afterChange', checkBg);
 //     checkBg();
 //
-    $('.header__slider-el').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true,
-        arrows: true,
-        prevArrow: $('.header__slider .slick-prev'),
-        nextArrow: $('.header__slider .slick-next'),
-        appendDots: $('.header__sliderDots')
-        // autoplay: true,
-        // autoplaySpeed: 3000,
-        // adaptiveHeight: true,
-    });
+//     $('.header__slider-el').slick({
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         dots: true,
+//         arrows: true,
+//         prevArrow: $('.header__slider .slick-prev'),
+//         nextArrow: $('.header__slider .slick-next'),
+//         appendDots: $('.header__sliderDots')
+//         // autoplay: true,
+//         // autoplaySpeed: 3000,
+//         // adaptiveHeight: true,
+//     });
 //
 //     var itemDetaliSlider = $('.item-detali-slider-view');
 //     var itemDetaliSliderPreview = $('.item-detali-slider-preview');
@@ -110,166 +110,65 @@ if($.fn.selectpicker){
         size: 7
     }).selectpicker('setStyle', 'btn', 'remove');
 }
-if ($.fn.magnificPopup) {
-    var magnificPopupObj = {
-        delegate: 'a',
-        type: 'image',
-        tLoading: "",
-        mainClass: "",
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0, 1]
-        },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-        },
-        iframe: {
-            // markup: '<div class="mfp-iframe-scaler">' +
-            // '<div class="mfp-close"></div>' +
-            // '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
-            // '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
-
-            patterns: {
-                youtube: {
-                    index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
-
-                    id: 'v=', // String that splits URL in a two parts, second part should be %id%
-                    // Or null - full URL will be returned
-                    // Or a function that should return %id%, for example:
-                    // id: function(url) { return 'parsed id'; }
-
-                    src: '//www.youtube.com/embed/%id%?autoplay=1' // URL that will be set as a source for iframe.
-                }
-            }
-        }
-    };
-
-    $('.magnificPopup').each(function () {
-        $(this).magnificPopup(magnificPopupObj);
-    })
-}
-
-// $('.catalog-category-list a').click(function(e){
-//     var _this = $(this),
-//         list = _this.next('ul'),
-//         parent = _this.parent(),
-//         className = 'active',
-//         time = 300;
+// if ($.fn.magnificPopup) {
+//     var magnificPopupObj = {
+//         delegate: 'a',
+//         type: 'image',
+//         tLoading: "",
+//         mainClass: "",
+//         gallery: {
+//             enabled: true,
+//             navigateByImgClick: true,
+//             preload: [0, 1]
+//         },
+//         image: {
+//             tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+//         },
+//         iframe: {
+//             // markup: '<div class="mfp-iframe-scaler">' +
+//             // '<div class="mfp-close"></div>' +
+//             // '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+//             // '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
 //
-//     if(list[0]){
-//         e.preventDefault();
-//         parent.toggleClass(className);
-//         list.slideToggle(time);
+//             patterns: {
+//                 youtube: {
+//                     index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
 //
-//         parent.siblings().removeClass(className)
-//             .find('li').removeClass(className)
-//             .end()
-//             .find('ul').slideUp(time);
-//     }
-// });
+//                     id: 'v=', // String that splits URL in a two parts, second part should be %id%
+//                     // Or null - full URL will be returned
+//                     // Or a function that should return %id%, for example:
+//                     // id: function(url) { return 'parsed id'; }
 //
-// var catalogFilterListToggleItem = $('.catalog-filter-list-toggle-item input');
+//                     src: '//www.youtube.com/embed/%id%?autoplay=1' // URL that will be set as a source for iframe.
+//                 }
+//             }
+//         }
+//     };
 //
-// catalogFilterListToggleItem.change(function(){
-//     var selector = catalogFilterListToggleItem.filter(':checked').val(),
-//         el = $("#"+selector),
-//         time = 300;
-//     if(el[0]){
-//         console.log(el);
-//         el.siblings().fadeOut(time, function(){
-//             el.fadeIn(time);
-//         });
-//     }
-// });
+//     $('.magnificPopup').each(function () {
+//         $(this).magnificPopup(magnificPopupObj);
+//     })
+// }
 
-$('.collapse').find('.collapse-toggle').click(function(e){
-    e.preventDefault();
-    var _this = $(this),
-        content = _this.next(),
-        parentContent = _this.closest('.collapse-toggle-content')[0]?_this.closest('.collapse-toggle-content'):_this.closest('.collapse'),
-        time = 300,
-        className = 'active';
+var bottomToggleMenu = $('#bottomToggleMenu'),
+    pageWrapper = $('.page__wrapper'),
+    pageWrapperClassShow = 'page__wrapper_show';
 
-    if(content.hasClass('collapse-toggle-content')){
-        content.slideToggle(time);
-        _this.parent().toggleClass(className);
+bottomToggleMenu.change(function () {
+    var checked = this.checked;
 
-        parentContent.find('.collapse-toggle-content').not(content).stop().slideUp(time);
-        parentContent.find('.collapse-toggle').not(_this).parent().removeClass(className);
+    if(checked){
+        pageWrapper.addClass(pageWrapperClassShow)
+    }else{
+        pageWrapper.removeClass(pageWrapperClassShow)
     }
 });
 
-var tableAlertData = $('.table-alert-data'),
-    tableAlertDataTable = $('.table-alert-data__table'),
-    tableAlertDataItem  = $(".table-alert-data__item"),
-    setTimeoutTableAlertDataTable = null;
-window.test = true;
 
-tableAlertDataTable.find('tbody > tr').mouseenter(function(){
-    var _this = $(this),
-        item = $("#"+_this.attr('data-item'));
-    clearTimeout(setTimeoutTableAlertDataTable);
-    setTimeoutTableAlertDataTable = setTimeout(function () {
-        if(window.test && item[0]){
-            var offsetTr = _this.offset().top,
-                offsetMain = tableAlertData.offset().top,
-                offsetTop = offsetTr-offsetMain;
-            _this.addClass('active').siblings().removeClass('active');
-            item.css({
-                'top': offsetTop+"px",
-                paddingTop: _this.height()+"px"
-            }).addClass('active').siblings().removeClass('active');
-        }
-    }, 400);
-});
 
-tableAlertData.mouseenter(function () {
-    window.test = true;
-}).mouseleave(function () {
-    tableAlertDataItem.add($(this).children()).removeClass('active');
-    window.test = false;
-});
-tableAlertDataItem.mouseleave(function () {
-    var _this = $(this),
-        id = _this.attr('id');
-    $(this).add('[data-item="'+id+'"]').removeClass('active');
-});
 
-var videoBlock = $('.videoBlock'),
-    videoBlockLabel = $('.videoBlock__label'),
-    videoBlockIframe = $('.videoBlock__main-iframe');
 
-videoBlockLabel.click(function(){
-    var _this = $(this);
 
-    _this.addClass('active').siblings().removeClass('active');
-    var iframe = _this.find('iframe');
-    if(iframe[0]){
-        videoBlockIframe.attr('src', iframe.attr('src'));
-    }
-});
 
-if($.fn.mCustomScrollbar){
-    var barScroll__content = $(".barScroll__content").mCustomScrollbar({
-        axis:"x",
-        autoExpandScrollbar:true,
-        advanced:{autoExpandHorizontalScroll:true},
-        mouseWheel: {
-            // enable: false,
-        }
-    });
 
-    var scrollLeft = 0;
-    $('.barScroll__btn').click(function () {
-        var _this = $(this);
-        // console.dir(barScroll__content.get(0).mcs);
-        if(_this.hasClass('barScroll__btn_left')){
-            scrollLeft -= 400;
-        }else{
-            scrollLeft += 400;
-        }
 
-        barScroll__content.mCustomScrollbar("scrollTo", scrollLeft);
-    });
-}
